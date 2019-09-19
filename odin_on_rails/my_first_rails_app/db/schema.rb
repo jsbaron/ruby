b191594f21fd6ae4b662ada26b53ee749b44917c
Josh Baron <baron.jos@husky.neu.edu>
2019-09-18 19:24:08 -0700

    restarted w new version of ruby

--- odin_on_rails/my_first_rails_app/db/schema.rb
+++ odin_on_rails/my_first_rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_013010) do
+ActiveRecord::Schema.define(version: 2019_09_19_022332) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
