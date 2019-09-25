6ee60477d949c020db18f56b4bc912242632e036
Josh Baron <baron.jos@husky.neu.edu>
2019-09-24 20:00:08 -0700

    finished guessing game

--- odin_on_rails/sinatra/web_guesser/web_guesser.rb
+++ odin_on_rails/sinatra/web_guesser/web_guesser.rb
@@ -2,7 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 num = rand(100)
+message = ""
 
 get '/' do
-    erb:index, :locals => {:num => num}
+    message = check_guess(params["guess"], num)
+    erb:index, :locals => {:num => num, :message => message}
+end
+
+def check_guess(guess, num)
+    guess = num_or_nil(guess)
+    if guess.nil?
+        message = "Invalid guess. Try again."
+    elsif guess > num
+        message = "#{guess} was too high! The correct number was #{num}."
+    elsif guess < num
+        message = "#{guess} was too low! The correct number was #{num}."
+    else guess == num
+        message = "Congrats! You guessed correctly! The correct number was #{num}."
+    end
+    return message
+end
+
+def num_or_nil(string)
+    int_guess = string.to_i
+    int_guess if int_guess.to_s == string
 end
